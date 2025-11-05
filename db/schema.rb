a2a4cc8d688f5cb4681e6c3fa7a9674b2f4f386e
Dave Kinkead <dave@kinkead.com.au>
2025-11-05 08:58:22 +0100

    Fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_05_072555) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_05_073148) do
+  create_table "accounts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "buckets", force: :cascade do |t|
     t.string "name", null: false
     t.string "description"
