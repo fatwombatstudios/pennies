d1a19f36d5b8e97f7501c885c021d26475f61a01
Dave Kinkead <dave@kinkead.com.au>
2025-11-08 10:49:56 +0100

    Update bucket spec

--- spec/models/bucket_spec.rb
+++ spec/models/bucket_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe Bucket, type: :model do
   it "has a valid factory" do
     expect(create :bucket).to be_valid
   end
+
+  it "lists buckets by type" do
+    income = create_list :bucket, 3, account_type: :income
+    spending = create_list :bucket, 3, account_type: :spending
+
+    expect(Bucket.income).to eq income
+    expect(Bucket.spending).to eq spending
+  end
 end
