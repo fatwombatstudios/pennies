d80bba4adf7845a6d8044e431fdbb767aad95e36
Dave Kinkead <dave@kinkead.com.au>
2025-11-08 10:53:19 +0100

    Update specs for #balance

--- spec/models/bucket_spec.rb
+++ spec/models/bucket_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Bucket, type: :model do
+  let(:account) { create :account }
+
   it "has a valid factory" do
     expect(create :bucket).to be_valid
   end
@@ -11,5 +13,59 @@ RSpec.describe Bucket, type: :model do
 
     expect(Bucket.income).to eq income
     expect(Bucket.spending).to eq spending
+  end
+
+  describe "#balance" do
+    context "for real accounts" do
+      it "calculates balance as debits - credits" do
+        real_bucket = create :bucket, account_type: :real, account: account
+        other_bucket = create :bucket, account_type: :spending, account: account
+
+        # Real account debited (money in)
+        create :entry, debit_account: real_bucket, credit_account: other_bucket, amount: 100, account: account
+
+        # Real account credited (money out)
+        create :entry, debit_account: other_bucket, credit_account: real_bucket, amount: 30, account: account
+
+        expect(real_bucket.balance).to eq 70 # 100 - 30
+      end
+    end
+
+    context "for virtual accounts" do
+      it "calculates balance as credits - debits" do
+        savings_bucket = create :bucket, account_type: :savings, account: account
+        other_bucket = create :bucket, account_type: :spending, account: account
+
+        # Savings account credited (money saved)
+        create :entry, debit_account: other_bucket, credit_account: savings_bucket, amount: 100, account: account
+
+        # Savings account debited (money withdrawn from savings)
+        create :entry, debit_account: savings_bucket, credit_account: other_bucket, amount: 30, account: account
+
+        expect(savings_bucket.balance).to eq 70 # 100 - 30
+      end
+    end
+  end
+
+  describe "#virtual?" do
+    it "returns true for income accounts" do
+      bucket = create :bucket, account_type: :income
+      expect(bucket.virtual?).to be true
+    end
+
+    it "returns true for savings accounts" do
+      bucket = create :bucket, account_type: :savings
+      expect(bucket.virtual?).to be true
+    end
+
+    it "returns true for spending accounts" do
+      bucket = create :bucket, account_type: :spending
+      expect(bucket.virtual?).to be true
+    end
+
+    it "returns false for real accounts" do
+      bucket = create :bucket, account_type: :real
+      expect(bucket.virtual?).to be false
+    end
   end
 end
